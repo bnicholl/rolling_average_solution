ffca2c1f8dc59d304492c82b036b7684ce9229a1
Ben Nicholl <bnicholl66@gmail.com>
2023-07-12 12:29:03 -0400

    Update 3_day_roll_ave.sql

--- 3_day_roll_ave.sql
+++ 3_day_roll_ave.sql
@@ -1,1 +1,27 @@
-WITH daily_totals AS (  SELECT    DATE(transaction_time) AS transaction_date,    SUM(transaction_amount) AS total_amount  FROM    transactions  GROUP BY    DATE(transaction_time)),rolling_averages AS (  SELECT    transaction_date,    AVG(total_amount) OVER (      ORDER BY transaction_date      ROWS BETWEEN 2 PRECEDING AND CURRENT ROW    ) AS rolling_average  FROM    daily_totals)SELECT  transaction_date,  rolling_averageFROM  rolling_averagesWHERE  transaction_date = '2021-01-31'
+WITH daily_totals AS (
+  SELECT
+    DATE(transaction_time) AS transaction_date,
+    SUM(transaction_amount) AS total_amount
+  FROM
+    transactions
+  GROUP BY
+    DATE(transaction_time)
+),
+rolling_averages AS (
+  SELECT
+    transaction_date,
+    AVG(total_amount) OVER (
+      ORDER BY transaction_date
+      ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS rolling_average
+  FROM
+    daily_totals
+)
+
+SELECT
+  transaction_date,
+  rolling_average
+FROM
+  rolling_averages
+WHERE
+  transaction_date = '2021-01-31'
